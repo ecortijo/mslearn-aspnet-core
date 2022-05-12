69f4606b1caa181e7af77dfbdb73309fcd820169
PLAINCONCEPTS\eduardo.cortijo <eduardo.cortijo@plainconcepts.com>
2022-05-12 13:38:08 +0200

    Fix previous commit

--- modules/microservices-data-aspnet-core/src/deploy/k8s/build-to-acr.sh
+++ modules/microservices-data-aspnet-core/src/deploy/k8s/build-to-acr.sh
@@ -56,7 +56,7 @@ do
 
     echo
     echo "Building image \"$image\" for service \"$service\" with \"$dockerfile.acr\"..."
-    az acr build -r $ESHOP_ACRNAME -t $ESHOP_REGISTRY/$image:linux-coupon -f $dockerfile.acr .
+    az acr build -r $ESHOP_ACRNAME -t $ESHOP_REGISTRY/$image:linux-net6-coupon -f $dockerfile.acr .
 done
 
 popd
